4e5d72025d220956692b58adb9ad473ff5731d81
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2024-07-17 08:06:20 -0700

    HC-522: Updated the start-verdi.sh to start verdi in either docker or podman (#4)
    * Updated the start-verdi.sh to run either podman or docker
    
    * Add temporary print statement
    
    * remove print statement
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- templates/start-verdi.sh
+++ templates/start-verdi.sh
@@ -5,5 +5,17 @@ export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 mkdir -p ${DATA_DIR}/work
 chown -R ${HOST_UID}:${HOST_GID} $DATA_DIR
-cd ${HOME}/verdi/ops/hysds-dockerfiles/verdi
-/usr/local/bin/docker-compose up -d
+
+# First, need to read in the celeryconfig to get the podman commands
+export PYTHONPATH=${HYSDS_HOME}/verdi/etc:${PYTHONPATH}
+
+container_engine=$(python -c "from hysds.celery import app; import json; print(app.conf.get('CONTAINER_ENGINE'))")
+
+cd ${HYSDS_HOME}/verdi/ops/hysds-dockerfiles/verdi
+if [[ "${container_engine}" == "docker" ]]; then
+  /usr/local/bin/docker-compose up -d
+elif [[ "${container_engine}" == "podman" ]]; then
+  ./run_verdi_podman.sh
+else
+  echo "ERROR: ${container_engine} not supported. Cannot start verdi container."
+fi
