0f1433845178b6bce55198dd6a01fc0207c8cae3
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-09-23 09:48:57 -0700

    Use start-verdi from code bundle deployment if it exists

--- templates/start-verdi.sh
+++ templates/start-verdi.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+
 export HYSDS_HOME=$HOME
-export DATA_DIR=/data
-export HOST_UID=$(id -u)
-export HOST_GID=$(id -g)
-mkdir -p ${DATA_DIR}/work
-chown -R ${HOST_UID}:${HOST_GID} $DATA_DIR
 
-# First, need to read in the celeryconfig to get the podman commands
-export PYTHONPATH=${HYSDS_HOME}/verdi/etc:${PYTHONPATH}
-
-container_engine=$(python -c "from hysds.celery import app; import json; print(app.conf.get('CONTAINER_ENGINE'))")
-
-cd ${HYSDS_HOME}/verdi/ops/hysds-dockerfiles/verdi
-if [[ "${container_engine}" == "docker" ]]; then
-  /usr/local/bin/docker-compose up -d
-elif [[ "${container_engine}" == "podman" ]]; then
-  ./run_verdi_podman.sh
+if [ -f ${HYSDS_HOME}/verdi/bin/start-verdi.sh ]; then
+  ${HYSDS_HOME}/verdi/bin/start-verdi.sh;
 else
-  echo "ERROR: ${container_engine} not supported. Cannot start verdi container."
+  export DATA_DIR=/data
+  export HOST_UID=$(id -u)
+  export HOST_GID=$(id -g)
+  mkdir -p ${DATA_DIR}/work
+  chown -R ${HOST_UID}:${HOST_GID} $DATA_DIR
+
+  # First, need to read in the celeryconfig to get the podman commands
+  export PYTHONPATH=${HYSDS_HOME}/verdi/etc:${PYTHONPATH}
+
+  container_engine=$(python -c "from hysds.celery import app; import json; print(app.conf.get('CONTAINER_ENGINE'))")
+
+  cd ${HYSDS_HOME}/verdi/ops/hysds-dockerfiles/verdi
+  if [[ "${container_engine}" == "docker" ]]; then
+    /usr/local/bin/docker-compose up -d
+  elif [[ "${container_engine}" == "podman" ]]; then
+    ./run_verdi_podman.sh
+  else
+    echo "ERROR: ${container_engine} not supported. Cannot start verdi container."
+  fi
 fi
